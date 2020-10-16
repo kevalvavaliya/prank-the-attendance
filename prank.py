f753a2fc16dc965d6e67b776ed8ab26608c10be0
kevalvavaliya <72963356+kevalvavaliya@users.noreply.github.com>
2020-10-16 12:42:54 +0530

    prank

---
+++ prank.py
@@ -0,0 +1,10 @@
+import requests
+import names
+i=1111111111
+url = 'https://docs.google.com/forms/u/0/d/e/YOUR FORM LINK WILL BE HERE WITH FORM RESPONSE/formResponse'
+while True:
+    i=i+1
+    rname = names.get_full_name()
+    form_data = {'entry.<ID/here i have enrollment no>':i,
+                'entry.<ID/random name>':rname}
+    r = requests.post(url, data=form_data)
